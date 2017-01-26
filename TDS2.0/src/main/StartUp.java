1e7c0804741178328706611ab43aaccabf519107
craftqq <nimdrop@t-online.de>
2017-01-26 20:40:00 +0100

    *Main -> StartUp +Base framework for game startup / end

---
+++ TDS2.0/src/main/StartUp.java
@@ -0,0 +1,56 @@
+package main;
+
+import java.io.InputStream;
+
+public class StartUp 
+{
+	private static final StartUp SINGLETON = new StartUp();
+	private StartUp(){}
+
+	public static void main(String[] args) 
+	{
+		preInit(args);
+		
+		init();
+		
+		postInit();
+		
+		exit();
+	}
+	
+	/**
+	 * creates the objects
+	 * @param args
+	 */
+	private static void preInit(String[] args)
+	{
+		
+	}
+	
+	/**
+	 * initializes the objects
+	 */
+	private static void init()
+	{
+		
+	}
+	
+	/**
+	 * prepares the game for user interaction
+	 */
+	private static void postInit()
+	{
+		
+	}
+	
+	private static void exit()
+	{
+		
+		System.exit(0);
+	}
+	
+	public static InputStream getResource(String resource)
+    {
+        return SINGLETON.getClass().getResourceAsStream(resource);
+    }
+}
