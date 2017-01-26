019e34873b04e313a17d3ca98f500e2d27f68bed
awsdcrafting <scisneromam@t-online.de>
2017-01-26 21:29:38 +0100

    +method load +method load(File file) +method load(String path)

--- TDS2.0/src/io/github/awsdcrafting/config/ConfigManager.java
+++ TDS2.0/src/io/github/awsdcrafting/config/ConfigManager.java
@@ -1,5 +1,35 @@
 package io.github.awsdcrafting.config;
 
-public class ConfigManager {
+import java.io.File;
 
+public class ConfigManager
+{
+	private File configFile;
+
+	public ConfigManager()
+	{
+		configFile = new File("Config.cfg");
+		
+	}
+
+	public void load()
+	{
+		
+		
+		load(configFile);
+	}
+	
+	public void load(String path){
+		
+		
+		load(new File(path));
+	}
+	
+	public void load(File file){
+		
+		
+		
+		
+	}
+	
 }
