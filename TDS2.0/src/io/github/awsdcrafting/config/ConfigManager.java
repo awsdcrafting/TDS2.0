efd4aeadf68ffb415d625aecf57544b2e24223fe
awsdcrafting <scisneromam@t-online.de>
2017-01-26 21:41:39 +0100

    *load(File file)

--- TDS2.0/src/io/github/awsdcrafting/config/ConfigManager.java
+++ TDS2.0/src/io/github/awsdcrafting/config/ConfigManager.java
@@ -1,35 +1,49 @@
 package io.github.awsdcrafting.config;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+
+import io.github.craftqq.FileIO.TextFileReader;
 
 public class ConfigManager
 {
-	private File configFile;
+	private File defaultConfigFile;
 
 	public ConfigManager()
 	{
-		configFile = new File("Config.cfg");
-		
+		defaultConfigFile = new File("Config.cfg");
+
 	}
 
 	public void load()
 	{
-		
-		
-		load(configFile);
+		load(defaultConfigFile);
 	}
-	
-	public void load(String path){
-		
-		
+
+	public void load(String path)
+	{
 		load(new File(path));
 	}
-	
-	public void load(File file){
-		
-		
-		
-		
+
+	public void load(File file)
+	{
+		String[] config = null;
+		try
+		{
+			config = TextFileReader.readFile(file);
+		} catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		for(int i = 0;i<config.length;i++){
+			if(config[i].startsWith("#")){
+				continue;
+			}
+			
+			
+			
+			
+		}
 	}
-	
+
 }
