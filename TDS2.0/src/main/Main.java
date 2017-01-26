43d34837a512ed715eb545a92448e7335cb3b37f
craftqq <nimdrop@t-online.de>
2017-01-26 18:29:17 +0100

    added Main class

---
+++ TDS2.0/src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Main 
+{
+	/**
+	 * DO NOT USE - MAIN IS A STATIC CLASS ONLY
+	 */
+	private Main(){}
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+	}
+
+}
